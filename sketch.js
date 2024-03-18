e1cfd6f04988faa196d2ed4d486adc31e84aeaab
Pranav Kulkarni <pranavkulkarni2104@gmail.com>
2024-03-18 19:53:12 -0400

    Sppp

--- sketch.js
+++ sketch.js
@@ -1,8 +1,8 @@
+let img
+function preload(){
+  img = loadImage('2160xDark.jpg',draw);
+}
 function setup() {
   createCanvas(500, 500);
-  loadImage('2160xDark.jpg',draw);
-}
-
-function draw(img) {
-  image(img,250,250,1000,1000);
+  image(img)
 }
