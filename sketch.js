dad1ad2119207aa901a1e51cf709b5010a88e3c0
Pranav Kulkarni <pranavkulkarni2104@gmail.com>
2024-03-18 19:11:52 -0400

    removed things

--- sketch.js
+++ sketch.js
@@ -1,10 +1,9 @@
 function setup() {
-  createCanvas(400, 500);
+  createCanvas(500, 500);
   loadImage('e3535c72855cb0b0819b75aa3e6fb2fe1866972a.webp',draw);
 }
 
 function draw(img) {
-  background(500);
   img.resize(100,100)
   image(img,0,0)
 }
