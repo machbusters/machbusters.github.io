2a81d5eb9b77d7e4a7446dc08f1dcdcd77490fb5
Pranav Kulkarni <pranavkulkarni2104@gmail.com>
2024-03-18 19:34:05 -0400

    Changed image format

--- sketch.js
+++ sketch.js
@@ -1,10 +1,9 @@
 function setup() {
   createCanvas(500, 500);
-  loadImage('e3535c72855cb0b0819b75aa3e6fb2fe1866972a.webp',draw);
+  loadImage('2160xDark.jpg',draw);
 }
 
 function draw(img) {
   img.resize(400,400);
   image(img,500,500);
-  draw(img)
 }
