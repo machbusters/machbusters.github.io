dd6e93940dffb58f363016e26bdf11004527677d
Pranav Kulkarni <pranavkulkarni2104@gmail.com>
2024-03-18 19:08:10 -0400

    size change

--- sketch.js
+++ sketch.js
@@ -1,9 +1,10 @@
 function setup() {
-  createCanvas(1000, 500);
+  createCanvas(500, 500);
   loadImage('e3535c72855cb0b0819b75aa3e6fb2fe1866972a.webp',draw);
 }
 
 function draw(img) {
   background(220);
+  img.resize(100,100)
   image(img,0,0)
 }
