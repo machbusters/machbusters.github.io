b060512b86bdfdc86fe6b85ec4e47f8842fb9f06
AU199 <149816958+AU199@users.noreply.github.com>
2024-03-19 19:28:37 -0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,10 @@
-let img;
-function preload(){
-  img = loadImage('2160xDark.jpg',draw);
-}
-function setup() {
-  createCanvas(500, 500);
-  img.resize(400,400);
-  image(img,250,250,400,400);
-}
+const checkmarks = [];
+const checkboxes = document.querySelectorAll('.note');
+
+getValueButton.addEventListener('click', function{
+
+  for(const checkbox in checkboxes){
+    if (checkbox.checked){
+    checkmarks.push(checkbox.value)
+  } 
+}});
