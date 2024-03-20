701a9788d9445c9ecee41ff92d19bc7348f3211f
Pranav Kulkarni <pranavkulkarni2104@gmail.com>
2024-03-20 19:14:17 -0400

    fixed funtion error

--- sketch.js
+++ sketch.js
@@ -1,10 +1,14 @@
 const checkmarks = [];
 const checkboxes = document.querySelectorAll('.note');
 
-getValueButton.addEventListener('click', function{
+getValueButton.addEventListener('click', function (){
 
   for(const checkbox in checkboxes){
     if (checkbox.checked){
     checkmarks.push(checkbox.value)
   } 
 }});
+
+function draw() {
+
+}
